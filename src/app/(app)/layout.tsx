a32afbd49b168e9f7e026edc645e3fd003167bf9
Flórián Lovasi <lovasifloriian@outlook.hu>
2025-01-18 13:06:25 +0100

    use env

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <GoogleTagManager gtmId="G-1QQC85JT7C"/>
+      <GoogleTagManager gtmId={process.env.GA_ID!}/>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
